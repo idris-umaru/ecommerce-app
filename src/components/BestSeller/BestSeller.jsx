4eb08f71e96c6bd895dddef5ff672c345f11f3c2
idris-umaru <idrisumaru123@gmail.com>
2026-03-24 17:48:38 +0100

    feat: implement BestSeller component

--- src/components/BestSeller/BestSeller.jsx
+++ src/components/BestSeller/BestSeller.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import React from 'react';
+import { products } from '../../data/product';
+import './BestSeller.css';
+import Title from '../Title/Title';
+import ProductItem from '../ProductItem/ProductItem';
 
 const BestSeller = () => {
-  return (
-    <div>BestSeller</div>
-  )
-}
 
-export default BestSeller
+  const bestSeller = products
+    .filter(item => item.bestseller === true)
+    .slice(0, 8);
+
+  return (
+    <div className='bestseller'>
+
+      <div className='bestseller-header'>
+        <Title text1={'BEST'} text2={'SELLERS'} />
+        <p className='bestseller-description'>
+          Our most loved products  handpicked by our customers.
+        </p>
+      </div>
+
+      <div className='bestseller-grid'>
+        {bestSeller.map(product => (
+          <ProductItem
+            key={product._id}
+            _id={product._id}
+            image={product.image}
+            name={product.name}
+            price={product.price}
+          />
+        ))}
+      </div>
+
+    </div>
+  );
+};
+
+export default BestSeller;
