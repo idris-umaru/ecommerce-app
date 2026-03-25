4f85eb4bde2f60eeea08fb889ef84365b56f10c0
idris-umaru <idrisumaru123@gmail.com>
2026-03-25 18:51:03 +0100

    feat: add BestCollection component to Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React, { useContext } from 'react';
 import { ShopContext } from '../context/ShopContext';
 import Hero from '../components/Hero/Hero';
 import LatestCollection from '../components/Collection/LatestCollection';
+import BestSeller from '../components/BestSeller/BestSeller';
 
 const Home = () => {
   
@@ -10,6 +11,7 @@ const Home = () => {
     <div>
       <Hero />
       <LatestCollection />
+      <BestSeller />
     </div>
   );
 };
