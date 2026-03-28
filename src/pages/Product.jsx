c437878534eed50659948a4621726cd94e99d6a4
idris-umaru <idrisumaru123@gmail.com>
2026-03-28 16:12:25 +0100

    feat: create Product page component

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,6 +1,11 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
 
 const Product = () => {
+  const {productId} = useParams()
+  console.log(productId)
+
+
   return (
     <div>Product</div>
   )
